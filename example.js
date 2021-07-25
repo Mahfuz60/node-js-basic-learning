2a49fbfbf4afd59cec82f7c473545bc4494805ce
Mahfuz <Mahfuz60>
2021-07-25 14:07:13 +0600

    code

---
+++ example.js
@@ -0,0 +1,10 @@
+// read and write stream
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream('./bigData.txt', 'utf8');
+    myReadStream.pipe(res);
+});
+server.listen(3000);
+console.log('server port 3000 is running now');
