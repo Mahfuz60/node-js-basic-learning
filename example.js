9b57a1ee16a115f0588f7f9147f46884c37f97d5
Mahfuz <Mahfuz60>
2021-07-26 22:27:05 +0600

    code

--- example.js
+++ example.js
@@ -6,5 +6,6 @@ const server = http.createServer((req, res) => {
     const myReadStream = fs.createReadStream('./bigData.txt', 'utf8');
     myReadStream.pipe(res);
 });
+// server connection
 server.listen(3000);
 console.log('server port 3000 is running now');
